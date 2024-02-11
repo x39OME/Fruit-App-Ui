1260bb912fddb183cc6fd1a80d65c648dd7ea12a
Sam <74021826+x39OME@users.noreply.github.com>
2024-02-11 20:31:19 +0300

    Add files via upload

---
+++ components/fruitCard.js
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { HeartIcon } from 'react-native-heroicons/solid'
+
+export default function fruitCard({fruit}) {
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  return (
+    <View style={{width:270, borderRadius:40, backgroundColor: fruit.color(1)}} className={`mx-4`}>
+      <View className='flex-row justify-end'>
+        <TouchableOpacity
+          onPress={()=> setIsFavourite(!isFavourite)}
+          className='p-3 rounded-full mr-4 mt-4'
+          style={{backgroundColor: 'rgba(255,255,255, .3)'}}>
+            <HeartIcon size='25' color={isFavourite? fruit.shadow:'white'} />
+          </TouchableOpacity>
+      </View>
+
+      <View className="flex-row justify-center" style={{
+          shadowColor: fruit.color,
+          shadowRadius: 40,
+          shadowOffset: {width: 0, height: 50},
+          shadowOpacity: 1,
+          elevation: 70,
+          
+          }}>
+          <Image source={fruit.image} style={{backgroundColor:'transparent', width: 210, height: 210}} />
+      </View>
+
+      <View className="ml-4 my-4">
+          <Text className="font-bold text-xl text-white shadow">{fruit.name}</Text>
+          <Text className="font-bold text-lg text-white shadow tracking-wide">$ {fruit.price}</Text>
+      </View>
+    </View>
+  )
+}
